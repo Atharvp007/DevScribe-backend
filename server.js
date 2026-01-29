1a07401ae3f006648c81cab3227f08735a700538
atharv pawar <atharvp721@gmail.com>
2026-01-30 01:39:39 +0530

    user routes

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./database/db.js"
+import userRoute from "./routes/user.route.js"
+
+
+dotenv.config()
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:true}));
+
+app.use("/api/v1/user", userRoute)
+
+app.listen(PORT, ()=>{
+    connectDB();
+    
+    console.log(`Server listen at port ${PORT}`);
+    
+})
