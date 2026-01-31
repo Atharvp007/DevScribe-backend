010ce35465bc62bcdb9c8f5d34729be257921d6f
atharv pawar <atharvp721@gmail.com>
2026-02-01 01:45:33 +0530

    cors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./database/db.js"
 import userRoute from "./routes/user.route.js"
-
+import cors from 'cors'
 
 dotenv.config()
 const app = express()
@@ -12,6 +12,11 @@ const PORT = process.env.PORT || 3000
 app.use(express.json());
 
 app.use(express.urlencoded({extended:true}));
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials:true
+}))
+
 
 app.use("/api/v1/user", userRoute)
 
