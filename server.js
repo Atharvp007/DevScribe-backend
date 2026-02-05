cfb67922b89abe4af9d0af499797bfc16957d868
atharv pawar <atharvp721@gmail.com>
2026-02-05 23:48:51 +0530

    blog

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv"
 import connectDB from "./database/db.js"
 import userRoute from "./routes/user.route.js"
 import cors from 'cors'
+import cookieParser from 'cookie-parser';
+import blogRoute from "./routes/blog.route.js"
 
 dotenv.config()
 const app = express()
@@ -10,6 +12,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
@@ -19,6 +22,7 @@ app.use(cors({
 
 
 app.use("/api/v1/user", userRoute)
+ app.use("/api/v1/blog", blogRoute)
 
 app.listen(PORT, ()=>{
     connectDB();
