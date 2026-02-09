32c7d74b3f6e544a78548a240acb154baf75dbb2
atharv pawar <atharvp721@gmail.com>
2026-02-09 23:17:34 +0530

    blogs

--- routes/blog.route.js
+++ routes/blog.route.js
@@ -1,10 +1,18 @@
 import express from "express"
 import { isAuthenticated } from "../middleware/isAuthenticated.js"
 import { singleUpload } from "../middleware/multer.js"
-import { createBlog, updateBlog  } from "../controllers/blog.controller.js"
+import { createBlog, updateBlog, getOwnBlogs, deleteBlog, likeBlog, dislikeBlog, getMyTotalBlogLikes, getPublishedBlog, togglePublishBlog } from "../controllers/blog.controller.js"
 
 const router = express.Router()
 
 router.route("/").post(isAuthenticated, createBlog)
 router.route("/:blogId").put(isAuthenticated, singleUpload, updateBlog)
+router.route("/get-own-blogs").get(isAuthenticated, getOwnBlogs)
+router.route("/delete/:id").delete(isAuthenticated, deleteBlog);
+router.get("/:id/like", isAuthenticated, likeBlog);
+router.get("/:id/dislike", isAuthenticated, dislikeBlog);
+router.get('/my-blogs/likes', isAuthenticated, getMyTotalBlogLikes)
+router.route("/get-published-blogs").get(getPublishedBlog)
+router.route("/:blogId").patch(togglePublishBlog);
+
 export default router
